1a8b00d4bb99cd1462368f2c6363ca0c272c0570
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-05-07 15:53:03 +0200

    fixup build

--- Resources/Scripts/enchilada.sh
+++ Resources/Scripts/enchilada.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Build an Android kernel that is actually UEFI disguised as the Kernel
-cat ./BootShim/BootShim.bin "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd" > "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd-bootshim"||exit 1
+cat ./BootShim/AARCH64/BootShim.bin "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd" > "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd-bootshim"||exit 1
 gzip -c < "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd-bootshim" > "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd-bootshim.gz"||exit 1
 cat "./Build/enchiladaPkg/${_TARGET_BUILD_MODE}_CLANGDWARF/FV/ENCHILADA_UEFI.fd-bootshim.gz" ./Resources/DTBs/enchilada.dtb > ./Resources/bootpayload.bin||exit 1
 
