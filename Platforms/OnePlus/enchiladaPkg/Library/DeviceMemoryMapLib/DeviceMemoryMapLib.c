cc8ad9b56b66a6c86ac64afd79afd96300092111
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-06-02 11:37:35 +0200

    Update DeviceMemoryMapLib.c

--- Platforms/OnePlus/enchiladaPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
+++ Platforms/OnePlus/enchiladaPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
@@ -7,6 +7,7 @@ gDeviceMemoryDescriptorEx[] = {
   // Name, Address, Length, HobOption, ResourceAttribute, ArmAttributes, ResourceType, MemoryType
 
 //--------------------- DDR  -----
+{"Kernel",            0x80000000, 0x05700000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
 {"Hypervisor",        0x85700000, 0x00600000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, NS_DEVICE},
 {"RAM Partition",     0x85700000, 0x00800000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
 {"HLOS1",             0x85F00000, 0x000C0000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
