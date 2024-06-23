f013f1c34efe2d9092e973ad6bb27ea2f02d632c
Daniel6745 <90069111+Daniel224455@users.noreply.github.com>
2024-06-23 20:28:42 +0200

    Update DeviceMemoryMapLib.c

--- Platforms/OnePlus/enchiladaPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
+++ Platforms/OnePlus/enchiladaPkg/Library/DeviceMemoryMapLib/DeviceMemoryMapLib.c
@@ -7,16 +7,16 @@ gDeviceMemoryDescriptorEx[] = {
   // Name, Address, Length, HobOption, ResourceAttribute, ArmAttributes, ResourceType, MemoryType
 
 //--------------------- DDR  -----
-{"Kernel",            0x80000000, 0x05700000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
-{"Hypervisor",        0x85700000, 0x00600000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
+{"Kernel",            0x80000000, 0x05700000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
+{"RAM Partition",     0x85700000, 0x00800000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
 {"HLOS1",             0x85F00000, 0x000C0000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
 {"RAM Partition",     0x85FC0000, 0x00020000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
 {"AOP CMD DB",        0x85FE0000, 0x00020000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
 {"SMEM",              0x86000000, 0x00200000, AddMem, MEM_RES, UNCACHEABLE, Reserv, UNCACHED_UNBUFFERED_XN},
 {"RAM Partition",     0x86200000, 0x11A00000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
-{"DXE Heap",          0xB0400000, 0x04500000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
-{"DBI Dump",          0x9D330000, 0x000D0000, NoHob, MMAP_IO, INITIALIZED, Conv, NS_DEVICE},
-{"Display Reserved",  0x9D400000, 0x02400000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_THROUGH_XN},
+{"DXE Heap",          0x97C00000, 0x05730000, AddMem, SYS_MEM, SYS_MEM_CAP, Conv, WRITE_BACK_XN},
+{"DBI Dump",          0x9D330000, 0x000D0000, NoHob, MMAP_IO, INITIALIZED, Conv, NS_DEVICE },
+{"Display Reserved",  0x9D400000, 0x02400000, AddMem, MEM_RES, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
 {"FV Region",         0x9F800000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK_XN},
 {"ABOOT FV",          0x9FA00000, 0x00200000, AddMem, SYS_MEM, SYS_MEM_CAP, Reserv, WRITE_BACK_XN},
 {"UEFI FD",           0x9FC00000, 0x00300000, AddMem, SYS_MEM, SYS_MEM_CAP, BsData, WRITE_BACK},
@@ -81,7 +81,6 @@ gDeviceMemoryDescriptorEx[] = {
 {"APSS_GIC500_GICR",   0x17A60000, 0x00100000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
 {"QTIMER",             0x17C00000, 0x00110000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
 {"OSM",                0x17D20000, 0x000B0000, AddDev, MMAP_IO, UNCACHEABLE, MmIO, NS_DEVICE},
-
 
   // Terminator for MMU
   {"Terminator", 0, 0, 0, 0, 0, 0, 0}
